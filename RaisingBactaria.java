48a770228250f021dd23e64d77d6f97bd8cbdb6c
DIPDAS4u <dipd2204@gmail.com>
2026-01-12 22:27:07 +0600

    Solve CF 579A- Raising Bacteria (JAVA)

---
+++ RaisingBactaria.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+public class RaisingBactaria {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int x = input.nextInt();
+        System.out.println(Integer.bitCount(x));
+    }
+}
